d57ea3e2ed604ea1cca0d461a3c9cc41df98175f
Prabesh <manandharprabesh@gmail.com>
2019-07-31 08:15:23 -0700

    Added SQL Server docker container

---
+++ ProductService/ProductService.Api/Infrastructure/Extensions/ExtensionMethods.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace ProductService.Api.Infrastructure.Extensions
+{
+    public class ExtensionMethods
+    {
+        public ExtensionMethods()
+        {
+        }
+    }
+}
